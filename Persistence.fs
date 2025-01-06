f469b885a17f7251ff0aca34c880d2e10a6b0d14
Max Gripe <108034506+MaxGripe@users.noreply.github.com>
2025-01-06 23:12:34 +0100

    Persistence fix

--- Persistence.fs
+++ Persistence.fs
@@ -6,9 +6,12 @@ open System.IO
 open System.Text.Json
 
 module Persistence =
-    let filePath = "tasks.json"
+    let filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "task-time-tracker", "tasks.json")
 
     let saveTasksToFile tasks =
+        let directory = Path.GetDirectoryName(filePath)
+        if not (Directory.Exists(directory)) then
+            Directory.CreateDirectory(directory) |> ignore
         let json = JsonSerializer.Serialize(tasks)
         File.WriteAllText(filePath, json)
 
@@ -18,3 +21,4 @@ module Persistence =
             JsonSerializer.Deserialize<(string * TimeSpan) list>(json)
         else
             [("New task", TimeSpan.Zero)]
+
